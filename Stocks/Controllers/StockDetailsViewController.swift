f0bb0b7e43fad6ad3ab64cb1d95d0c4d933255ab
Alex Dinh <anhdinh17@gmail.com>
2021-07-24 23:05:30 -0500

    Started the project

---
+++ Stocks/Controllers/StockDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockDetailsViewController.swift
+//  Stocks
+//
+//  Created by Anh Dinh on 7/24/21.
+//
+
+import UIKit
+
+class StockDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ Stocks/Controllers/WatchListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Anh Dinh on 7/24/21.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
